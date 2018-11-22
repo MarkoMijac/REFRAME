1a5d086fa14619fdeff789f1b175e457ef9bef9e
mmijac <mmijac@foi.hr>
2018-11-22 16:24:47 +0100

    Implement basic reactive node abstractions

---
+++ ReframeCore/ReframeCore/INode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCore
+{
+    /// <summary>
+    /// Interface describing reactive node.
+    /// </summary>
+    public interface INode
+    {
+        string MemberName { get; }
+        object OwnerObject { get; }
+        void Update();
+    }
+}
---
+++ ReframeCore/ReframeCore/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCore
+{
+    /// <summary>
+    /// Reactive node which encapsulates object's property that needs to be tracked.
+    /// </summary>
+    public class Node : INode
+    {
+        /// <summary>
+        /// The class member (property or method) reactive node represents.
+        /// </summary>
+        public string MemberName { get; private set; }
+
+        /// <summary>
+        /// An associated object which owns the member.
+        /// </summary>
+        public object OwnerObject { get; private set; }
+
+        public Node(string memberName, object ownerObject)
+        {
+            MemberName = memberName;
+            OwnerObject = ownerObject;
+        }
+
+        /// <summary>
+        /// A method responsible for updating reactive node.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+}
