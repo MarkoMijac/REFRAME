f146ce6ce566e37b14a32edca18375e6a50836c7
mmijac <mmijac@foi.hr>
2019-02-13 10:14:35 +0100

    Add example Whole-Part for CollectionNode testing

---
+++ ReframeCore/ReframeCoreExamples/E07/Part.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCoreExamples.E07
+{
+    public class Part
+    {
+        #region Properties
+
+        public string Name { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private void Update_A()
+        {
+
+        }
+
+        private void Update_B()
+        {
+
+        }
+
+        private void Update_C()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ ReframeCore/ReframeCoreExamples/E07/Whole.cs
@@ -0,0 +1,63 @@
+﻿using ReframeCore.ReactiveCollections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCoreExamples.E07
+{
+    public class Whole
+    {
+        #region Properties
+
+        public ReactiveCollection<Part> Parts { get; set; }
+
+        public string Name { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public Whole()
+        {
+            Parts = new ReactiveCollection<Part>();
+            Parts.Add(new Part { Name = "Part 1", A = 1, B = 2, C = 3});
+            Parts.Add(new Part { Name = "Part 2", A = 4, B = 5, C = 6 });
+            Parts.Add(new Part { Name = "Part 3", A = 7, B = 8, C = 9 });
+        }
+
+        private void Update_A()
+        {
+            A = 0;
+            foreach (var p in Parts)
+            {
+                A += p.A;
+            }
+        }
+
+        private void Update_B()
+        {
+            B = 0;
+            foreach (var p in Parts)
+            {
+                B += p.B;
+            }
+        }
+
+        private void Update_C()
+        {
+            C = 0;
+            foreach (var p in Parts)
+            {
+                C += p.C;
+            }
+        }
+
+        #endregion
+
+    }
+}
