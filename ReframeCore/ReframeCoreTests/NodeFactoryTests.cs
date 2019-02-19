d16d05d73ac58cc80b28a7f0db1d926daf6f770d
mmijac <mmijac@foi.hr>
2019-02-19 17:14:55 +0100

    Add unit tests for NodeFactory CreateNode method for adding CollectionNodes.

--- ReframeCore/ReframeCoreTests/NodeFactoryTests.cs
+++ ReframeCore/ReframeCoreTests/NodeFactoryTests.cs
@@ -5,62 +5,62 @@ using ReframeCore.Nodes;
 using ReframeCore;
 using ReframeCore.Exceptions;
 using ReframeCoreExamples.E02;
+using ReframeCoreExamples.E07;
+using ReframeCore.ReactiveCollections;
 
 namespace ReframeCoreTests
 {
     [TestClass]
     public class NodeFactoryTests
     {
+        private NodeFactory defaultFactory = new NodeFactory();
+
         #region PropertyNode
 
         [TestMethod]
         public void CreateNode_GivenPropertyOwnerObjectIsNull_ThrowsException()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building00 building00 = null;
             string memberName = "Width";
 
             //Act & Assert
-            Assert.ThrowsException<ReactiveNodeException>(() => nodeFactory.CreateNode(building00, memberName));
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(building00, memberName));
         }
 
         [TestMethod]
         public void CreateNode_GivenWrongPropertyName_ThrowsException()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building00 building00 = new Building00();
             string memberName = "Width_Wrong";
 
             //Act & Assert
-            Assert.ThrowsException<ReactiveNodeException>(() => nodeFactory.CreateNode(building00, memberName));
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(building00, memberName));
         }
 
         [TestMethod]
         public void CreateNode_GivenWrongUpdateMethodIsProvided_ThrowsException()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building00 building00 = new Building00();
             string propertyName = "Area";
             string updateMethodName = "Update_Area_Wrong";
 
             //Act & Assert
-            Assert.ThrowsException<ReactiveNodeException>(() => nodeFactory.CreateNode(building00, propertyName, updateMethodName));
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(building00, propertyName, updateMethodName));
         }
 
         [TestMethod]
         public void CreateNode_GivenCorrectUpdateMethodIsProvided_ReturnsPropertyNodeWithUpdateMethod()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building00 building = new Building00();
             string propertyName = "Area";
             string updateMethodName = "Update_Area";
 
             //Act
-            PropertyNode node = nodeFactory.CreateNode(building, propertyName, updateMethodName) as PropertyNode;
+            PropertyNode node = defaultFactory.CreateNode(building, propertyName, updateMethodName) as PropertyNode;
 
             //Assert
             Assert.IsTrue(node.OwnerObject == building
@@ -130,36 +130,33 @@ namespace ReframeCoreTests
         public void CreateNode_GivenMethodOwnerObjectIsNull_ThrowsException()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building00 building02 = null;
             string methodName = "Update_Area";
 
             //Act & Assert
-            Assert.ThrowsException<ReactiveNodeException>(() => nodeFactory.CreateNode(building02, methodName));
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(building02, methodName));
         }
 
         [TestMethod]
-        public void CreateNode_GivenIncorrectMethodName_ReturnsMethodNode()
+        public void CreateNode_GivenIncorrectMethodName_ThrowsException()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building02 building = new Building02();
             string methodName = "Update_Area_Wrong";
 
             //Act & Assert
-            Assert.ThrowsException<ReactiveNodeException>(() => nodeFactory.CreateNode(building, methodName));
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(building, methodName));
         }
 
         [TestMethod]
         public void CreateNode_GivenCorrectArguments_ReturnsMethodNode()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building02 building = new Building02();
             string methodName = "Update_Area";
 
             //Act
-            INode updateAreaNode = nodeFactory.CreateNode(building, methodName);
+            INode updateAreaNode = defaultFactory.CreateNode(building, methodName);
 
             //Assert
             Assert.IsInstanceOfType(updateAreaNode, typeof(MethodNode));
@@ -169,17 +166,101 @@ namespace ReframeCoreTests
         public void CreateNode_GivenCorrectArguments_ReturnsMethodnoNodeWithCorrectData()
         {
             //Arrange
-            NodeFactory nodeFactory = new NodeFactory();
             Building02 building = new Building02();
             string memberName = "Update_Area";
 
             //Act
-            MethodNode updateAreaNode = nodeFactory.CreateNode(building, memberName) as MethodNode;
+            MethodNode updateAreaNode = defaultFactory.CreateNode(building, memberName) as MethodNode;
 
             //Assert
             Assert.IsTrue(updateAreaNode.OwnerObject == building
                 && updateAreaNode.MemberName == memberName
                 && updateAreaNode.UpdateMethod.Method.Name == memberName);
+        }
+
+        #endregion
+
+        #region CollectionNode
+
+        [TestMethod]
+        public void CreateNode_GivenCorrectArguments_ReturnsCollectionNode()
+        {
+            //Arrange
+            Whole whole = new Whole { Name = "Whole 1" };
+
+            //Act
+            CollectionNode<Part> partsNode = defaultFactory.CreateNode(whole.Parts, "A") as CollectionNode<Part>;
+
+            //Assert
+            Assert.IsInstanceOfType(partsNode, typeof(CollectionNode<Part>));
+        }
+
+        [TestMethod]
+        public void CreateNode_CreatedCollectionNode_HoldsProperData()
+        {
+            //Arrange
+            Whole whole = new Whole { Name = "Whole 1" };
+            ReactiveCollection<Part> parts = whole.Parts;
+
+            //Act
+            CollectionNode<Part> partsNode = defaultFactory.CreateNode(parts, "A") as CollectionNode<Part>;
+
+            //Assert
+            Assert.IsTrue(partsNode.OwnerObject == parts && partsNode.MemberName == "A");
+        }
+
+        [TestMethod]
+        public void CreateNode_SourceNodeUpdateInvoke_DoesNotBreak()
+        {
+            //Arrange
+            Whole whole = new Whole { Name = "Whole 1" };
+
+            try
+            {
+                //Act
+                CollectionNode<Part> partsNode = defaultFactory.CreateNode(whole.Parts, "A") as CollectionNode<Part>;
+                partsNode.Update();
+            }
+            catch (Exception ex)
+            {
+                //Assert
+                Assert.Fail("Expected no exception, instead got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CreateNode_GivenNullObject_ThrowsException()
+        {
+            //Arrange
+            Whole whole = new Whole { Name = "Whole 1" };
+            ReactiveCollection<Part> parts = null;
+
+            //Act & Assert
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(parts, "A") as CollectionNode<Part>);
+        }
+
+        [TestMethod]
+        public void CreateNode_GivenEmptyReactiveCollection_CreatesNode()
+        {
+            //Arrange
+            ReactiveCollection<Part> collection = new ReactiveCollection<Part>();
+
+            //Act
+            CollectionNode<Part> node = defaultFactory.CreateNode(collection, "A") as CollectionNode<Part>;
+
+            //Assert
+            //Assert
+            Assert.IsInstanceOfType(node, typeof(INode));
+        }
+
+        [TestMethod]
+        public void CreateNode_GivenNonExistantMemberName_ThrowsException()
+        {
+            //Arrange
+            Whole whole = new Whole { Name = "Whole 1" };
+
+            //Act & Assert
+            Assert.ThrowsException<ReactiveNodeException>(() => defaultFactory.CreateNode(whole.Parts, "AA") as CollectionNode<Part>);
         }
 
         #endregion
