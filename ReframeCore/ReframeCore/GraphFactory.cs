c2f6be4d6f69e7461f47340f0edf431e212ff13e
mmijac <mmijac@foi.hr>
2019-02-26 16:53:26 +0100

    Implement GraphFactory class and add unit tests.

---
+++ ReframeCore/ReframeCore/GraphFactory.cs
@@ -0,0 +1,79 @@
+﻿using ReframeCore.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCore
+{
+    public static class GraphFactory
+    {
+        private static List<DependencyGraph> _graphs = new List<DependencyGraph>();
+
+        /// <summary>
+        /// Creates and registers new dependency graph with unique identifier.
+        /// </summary>
+        /// <param name="identifier">Graph's unique identifier.</param>
+        /// <returns>New dependency graph.</returns>
+        public static IDependencyGraph Create(string identifier)
+        {
+            ValidateIdentifier(identifier);
+            
+            var graph = new DependencyGraph(identifier);
+            _graphs.Add(graph);
+            return graph;
+        }
+
+        private static void ValidateIdentifier(string identifier)
+        {
+            if (identifier == "")
+            {
+                throw new DependencyGraphException("Identifier for dependency graph cannot be empty!");
+            }
+            else if (CheckIfGraphAlreadyExists(identifier))
+            {
+                throw new DependencyGraphException("Dependency graph cannot be created! Dependency graph with identifier " + identifier + " already exists!");
+            }
+        }
+
+        private static bool CheckIfGraphAlreadyExists(string identifier)
+        {
+            return _graphs.Any(g => g.Identifier == identifier);
+        }
+
+        /// <summary>
+        /// Returns registered graph with provided identifier if exists.
+        /// </summary>
+        /// <param name="identifier">Graph identifier.</param>
+        /// <returns>Dependency graph if exists, otherwise null.</returns>
+        public static IDependencyGraph Get(string identifier)
+        {
+            return _graphs.FirstOrDefault(g => g.Identifier == identifier);
+        }
+
+        /// <summary>
+        /// Gets graph if there is one registered with provided identifier. Otherwise it creates one.
+        /// </summary>
+        /// <param name="identifier">Graph identifier.</param>
+        /// <returns>Existing or newly created dependency graph.</returns>
+        public static IDependencyGraph GetOrCreate(string identifier)
+        {
+            var graph = Get(identifier);
+            if (graph == null)
+            {
+                graph = Create(identifier);
+            }
+
+            return graph;
+        }
+
+        /// <summary>
+        /// Clears all dependency graphs.
+        /// </summary>
+        public static void Clear()
+        {
+            _graphs.Clear();
+        }
+    }
+}
---
+++ ReframeCore/ReframeCoreTests/GraphFactoryTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReframeCore;
+using ReframeCore.Exceptions;
+
+namespace ReframeCoreTests
+{
+    [TestClass]
+    public class GraphFactoryTests
+    {
+        #region Create
+
+        [TestMethod]
+        public void Create_GivenNoOtherDependencyGraphsExist_CreatesNewDependencyGraph()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "G1";
+
+            //Act
+            var graph = GraphFactory.Create(identifier);
+
+            //Assert
+            Assert.IsTrue(graph != null && graph.Identifier == identifier);
+        }
+
+        [TestMethod]
+        public void Create_GivenUniqueIdentifierProvided_CreatesNewDependencyGraph()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            GraphFactory.Create("G1");
+
+            //Act
+            var graph = GraphFactory.Create("G2");
+
+            //Assert
+            Assert.IsNotNull(graph);
+        }
+
+        [TestMethod]
+        public void Create_GivenAlreadyTakenIdentifierProvided_ThrowsException()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            var graph = GraphFactory.Create("G1");
+
+            //Act & Assert
+            Assert.ThrowsException<DependencyGraphException>(() => GraphFactory.Create("G1"));
+        }
+
+        [TestMethod]
+        public void Create_GivenEmptyStringIdentifier_ThrowsException()
+        {
+            //Arrange
+            GraphFactory.Clear();
+
+            //Act & Assert
+            Assert.ThrowsException<DependencyGraphException>(() => GraphFactory.Create(""));
+        }
+
+        #endregion
+
+        #region Get
+
+        [TestMethod]
+        public void Get_GivenGraphWithProvidedIdentifierExists_ReturnsDependencyGraph()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "G1";
+            GraphFactory.Create(identifier);
+
+            //Act
+            var graph = GraphFactory.Get(identifier);
+
+            //Assert
+            Assert.IsNotNull(graph);
+        }
+
+        [TestMethod]
+        public void Get_GivenGraphWithProvidedIdentifierDoesNotExist_ReturnsNull()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "G1";
+            string nonexistant = "G2";
+            GraphFactory.Create(identifier);
+
+            //Act
+            var graph = GraphFactory.Get(nonexistant);
+
+            //Assert
+            Assert.IsNull(graph);
+        }
+
+        #endregion
+
+        #region GetOrCreate
+
+        [TestMethod]
+        public void GetOrCreate_GivenGraphWithProvidedIdentifierExists_ReturnsExistingDependencyGraph()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "G1";
+            GraphFactory.Create(identifier);
+
+            //Act
+            var graph = GraphFactory.GetOrCreate(identifier);
+
+            //Assert
+            Assert.IsTrue(graph.Identifier == identifier);
+        }
+
+        [TestMethod]
+        public void GetOrCreate_GivenGraphWithProvidedIdentifierDoesNotExist_ReturnsNewDependencyGraph()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "G1";
+
+            //Act
+            var graph = GraphFactory.GetOrCreate(identifier);
+
+            //Assert
+            Assert.IsTrue(graph.Identifier == identifier);
+        }
+
+        [TestMethod]
+        public void GetOrCreate_GivenEmptyStringIdentifier_ThrowsException()
+        {
+            //Arrange
+            GraphFactory.Clear();
+            string identifier = "";
+
+            //Act & Assert
+            Assert.ThrowsException<DependencyGraphException>(() => GraphFactory.GetOrCreate(identifier));
+        }
+
+        #endregion
+    }
+}
