d986c8032b08f90e31e96136497d21b8d1dede2c
Marko Mijac <mijac.marko@gmail.com>
2019-03-05 15:23:53 +0100

    Add unit tests for CollectionNode simple scenarios.

---
+++ ReframeCore/ReframeCoreExamples/E08/E3/OrderItem_8_3.cs
@@ -0,0 +1,51 @@
+﻿using ReframeCore;
+using ReframeCore.ReactiveCollections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCoreExamples.E08.E3
+{
+    public class OrderItem_8_3 : ICollectionNodeItem
+    {
+        private IDependencyGraph _graph;
+        public event EventHandler UpdateTriggered;
+
+
+        private double _fixedValue;
+
+        public double FixedValue
+        {
+            get { return _fixedValue; }
+            set { _fixedValue = value; }
+        }
+
+
+        private double _total = 1;
+
+        public double Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+            }
+        }
+
+        public Order_8_3 Order { get; set; }
+
+        public OrderItem_8_3(Order_8_3 order)
+        {
+            _graph = GraphFactory.Get("GRAPH_8_3");
+            Order = order;
+        }
+
+        private void Update_Total()
+        {
+            Total = FixedValue * (1 - (Order.DiscountA + Order.DiscountB)/100);
+        }
+
+    }
+}
---
+++ ReframeCore/ReframeCoreExamples/E08/E4/Order_8_4.cs
@@ -0,0 +1,80 @@
+﻿using ReframeCore;
+using ReframeCore.ReactiveCollections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeCoreExamples.E08.E4
+{
+    public class Order_8_4
+    {
+        private IDependencyGraph _graph;
+
+        private double _total;
+
+        public double Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        private double _totalVAT;
+
+        public double TotalVAT
+        {
+            get { return _totalVAT; }
+            set { _totalVAT = value; }
+        }
+
+        private double _discountA = 0;
+
+        public double DiscountA
+        {
+            get { return _discountA; }
+            set
+            {
+                _discountA = value;
+                _graph.PerformUpdate(this, "DiscountA");
+            }
+        }
+
+        private double _discountB = 0;
+        public double DiscountB
+        {
+            get { return _discountB; }
+            set
+            {
+                _discountB = value;
+                _graph.PerformUpdate(this, "DiscountB");
+            }
+        }
+
+        public ReactiveCollection<OrderItem_8_4> Items { get; set; }
+
+        public Order_8_4()
+        {
+            _graph = GraphFactory.Get("GRAPH_8_4");
+            Items = new ReactiveCollection<OrderItem_8_4>();
+        }
+
+        private void Update_Total()
+        {
+            Total = 0;
+            foreach (var item in Items)
+            {
+                Total += item.Total;
+            }
+        }
+
+        private void Update_TotalVAT()
+        {
+            TotalVAT = 0;
+            foreach (var item in Items)
+            {
+                TotalVAT += item.Total * 1.25;
+            }
+        }
+    }
+}
