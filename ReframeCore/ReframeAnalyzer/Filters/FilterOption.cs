4c7dc6ca2803563dedfe583591b96cad4a0b7348
mmijac <mmijac>
2020-08-06 10:39:33 +0200

    Implement FilterOption class...

---
+++ ReframeCore/ReframeAnalyzer/Filters/FilterOption.cs
@@ -0,0 +1,50 @@
+﻿using ReframeAnalyzer.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeAnalyzer.Filters
+{
+    public abstract class FilterOption : IFilterOption
+    {
+        private List<IAnalysisNode> SelectedNodes { get; set; } = new List<IAnalysisNode>();
+
+        public bool IsSelected(IAnalysisNode node)
+        {
+            return SelectedNodes.Exists(n => n.Identifier == node.Identifier);
+        }
+
+        public void SelectNode(IAnalysisNode node)
+        {
+            if (node != null && IsSelected(node) == false)
+            {
+                SelectedNodes.Add(node);
+            }
+        }
+
+        public void DeselectNode(IAnalysisNode node)
+        {
+            if (node != null && IsSelected(node) == true)
+            {
+                SelectedNodes.Remove(node);
+            }
+        }
+
+        public abstract List<IAnalysisNode> GetAvailableNodes();
+
+        public void SelectAllNodes()
+        {
+            foreach (var node in GetAvailableNodes())
+            {
+                SelectNode(node);
+            }
+        }
+
+        public void DeselectAllNodes()
+        {
+            SelectedNodes.Clear();
+        }
+    }
+}
---
+++ ReframeCore/ReframeAnalyzer/Filters/IFilterOption.cs
@@ -0,0 +1,19 @@
+﻿using ReframeAnalyzer.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReframeAnalyzer.Filters
+{
+    interface IFilterOption
+    {
+        List<IAnalysisNode> GetAvailableNodes();
+        void SelectAllNodes();
+        void DeselectAllNodes();
+        void SelectNode(IAnalysisNode node);
+        void DeselectNode(IAnalysisNode node);
+        bool IsSelected(IAnalysisNode node);
+    }
+}
